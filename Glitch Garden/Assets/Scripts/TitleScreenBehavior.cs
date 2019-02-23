9e12106a57744603eeb82a0eba173cbb70feb026
Robert Lafferty <rclafferty@ksu.edu>
2019-02-23 00:45:38 -0600

    Added Splash Screen and Title Screen

---
+++ Glitch Garden/Assets/Scripts/TitleScreenBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreenBehavior : MonoBehaviour
+{
+    Sprite[] backgroundImages;
+    Image backgroundRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundRenderer = GameObject.Find("Background Image").GetComponent<Image>();
+        backgroundImages = Resources.LoadAll<Sprite>("Images/TitleScreenImages");
+        int randomIndex = Random.Range(0, backgroundImages.Length - 1);
+        backgroundRenderer.sprite = backgroundImages[randomIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
